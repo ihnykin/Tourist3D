602b7a3b97a9eed97fbacd6e532cf78c3a2f1a7f
ihnykin <i.hnykin@socialquantum.com>
2020-10-16 19:31:29 +0300

    collection targets + ,,,

---
+++ Tourist3D/Source/Tourist3D/Utils/CommonUtils.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "CommonUtils.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOURIST3D_API UCommonUtils : public UObject
+{
+    GENERATED_BODY()
+
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Common Utils")
+    static bool IsDebugBuild();
+};
